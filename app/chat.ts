b391f3efb26aaca5c945b7fe00d11e4cf360c578
ManfredSteyer <manfred.steyer@gmx.net>
2018-09-13 13:04:51 +0200

    observables

--- app/chat.ts
+++ app/chat.ts
@@ -1,37 +1,80 @@
-export const conn = new WebSocket('wss://remy-ws.glitch.me/');
+import { Observable, Observer, fromEvent } from 'rxjs';
+import { map, filter } from 'rxjs/operators';
+//                             ^--- Ab v 6 normal
+//                                  davor: Anti-Pattern
+
+export class Chat {
+
+    messages$: Observable<string>;
+
+    private conn: WebSocket;
+
+    connect() {
+        
+        this.messages$ = Observable.create((sender: Observer<string>) => {
+
+            this.conn = new WebSocket('wss://remy-ws.glitch.me/');
+
+            this.conn.onopen = event => console.debug('open');    
+
+            this.conn.onmessage = function (event) {
+                const message = event.data; 
+                sender.next(message);
+            };
+            
+            this.conn.onclose = function (event) {
+                sender.complete();
+            };
+            
+            this.conn.onerror = function (event) {
+                sender.error(event);
+            };
+
+            return () => {
+                this.conn.close();
+            }
+    
+        });
+
+        
+
+    }
+
+    send(message: string): void {
+        this.conn.send(message);
+    }
+
+}
 
 const output = document.getElementById('output');
 
-conn.onopen = event => console.debug('open');    
 
-conn.onmessage = function (event) {
-    const message = event.data; 
-    output.innerHTML += `<li>
-                            ${message}
-                        </li>`; // Es5
-};
+const chat = new Chat();
+chat.connect();
+chat.messages$
+    .pipe(
+        filter(msg => isNaN(parseFloat(msg))),
+        map(msg => `<b>${msg}</b>`)
+    )
+    .subscribe(
+    message => {
+        output.innerHTML += `<li>${message}</li>`;
+    }
+);
 
-conn.onclose = function (event) {
-    console.debug('closed');
-};
-
-conn.onerror = function (event) {
-    console.debug('error');
-};
 
 const input = document.getElementById("input") as HTMLInputElement;
 
-input.addEventListener('keydown', e => {
-    if (e.key === 'Enter') {
-        const msg = input.value;
-        
-        conn.send(msg);
-
-        output.innerHTML += `<li>ME> ${msg}</li>`;
-
-        input.value = '';
+fromEvent(input, 'keydown').pipe(
+    map((e: KeyboardEvent) => [e.key, input.value])
+)
+.subscribe(
+    tupel => {
+        if (tupel[0] === 'Enter') {
+            const msg = tupel[1];
+            chat.send(msg);
+            output.innerHTML += `<li>ME> ${msg}</li>`;
+            input.value = '';
+        }
     }
-})
-
-
-
+);
