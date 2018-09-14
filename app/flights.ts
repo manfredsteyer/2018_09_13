0d611fc51d0ff5ea7bbb450bf9b87eab96395fe3
ManfredSteyer <manfred.steyer@gmx.net>
2018-09-14 09:48:45 +0200

    aufgeräumt

--- app/flights.ts
+++ app/flights.ts
@@ -1,49 +1,42 @@
 import { from, Observable, Observer, fromEvent, combineLatest, Subject, BehaviorSubject, ReplaySubject } from "rxjs";
-//                                                  ^------ Factory-Functionen
-
 import { publish, map, debounceTime, switchMap, flatMap, delay, exhaustMap, share } from "rxjs/operators";
-//                                                                ^------- pipe
 import { Flight } from "./flight";
-
-// obs.pipe(combineLatest(obs2)) <==> combineLatest(obs, obs2)
-
-function find(from: string, to: string): Observable<Flight[]> {
-    return Observable.create((sender: Observer<string>) => {
-        fetch(`http://www.angular.at/api/flight?from=${from}&to=${to}`)
-            .then(r => r.text())
-            .then(text => sender.next(text))
-            .catch(e => sender.error(e));
-    }).pipe(
-        map( (txt: string) => JSON.parse(txt)),
-        // delay(5000)
-    );
-}
-
 
 // Quellen
 
-let flightSelected$ = new BehaviorSubject<Flight>(null);
+const flightSelected$ = new BehaviorSubject<Flight>(null);
 
-let from$ = fromEvent(document.getElementById('from'), 'input')
+const from$ = fromEvent(document.getElementById('from'), 'input')
                 .pipe(
                     debounceTime(300),
                     map( (e: any ) => e.target.value)
                 );
 
-let to$ = fromEvent(document.getElementById('to'), 'input')
+const to$ = fromEvent(document.getElementById('to'), 'input')
                 .pipe(
                     debounceTime(300),
                     map( (e: any ) => e.target.value)
                 );
 
-let flights$ = combineLatest(from$, to$)
+const btnRegister = document.getElementById('btnRegister');
+fromEvent(btnRegister, 'click').subscribe(e => {
+    
+    flightSelected$.subscribe(flight => {
+        console.debug('Flight selected', flight);
+    });
+
+});
+
+// Senken
+
+const flights$ = combineLatest(from$, to$)
                     .pipe(
                         map(t => ({from: t[0], to: t[1]})),
                         switchMap(p => find(p.from, p.to)),
                         share()
                     );
 
-let count$ = flights$.pipe(map(flights => flights.length));
+const count$ = flights$.pipe(map(flights => flights.length));
 
 flights$.subscribe(
     flights => render(flights),
@@ -56,13 +49,7 @@ count$.subscribe(
 );
 
 
-const btnRegister = document.getElementById('btnRegister');
-fromEvent(btnRegister, 'click').subscribe(e => {
-    flightSelected$.subscribe(flight => {
-        console.debug('Flight selected', flight);
-    });
-});
-
+// Hilfskonstrukte
 
 function render(flights: Flight[]) {
     let html = '<table class="table table-striped">';
@@ -81,4 +68,17 @@ function render(flights: Flight[]) {
             flightSelected$.next(f);
         });
     });
+}
+
+
+function find(from: string, to: string): Observable<Flight[]> {
+    return Observable.create((sender: Observer<string>) => {
+        fetch(`http://www.angular.at/api/flight?from=${from}&to=${to}`)
+            .then(r => r.text())
+            .then(text => sender.next(text))
+            .catch(e => sender.error(e));
+    }).pipe(
+        map( (txt: string) => JSON.parse(txt)),
+        // delay(5000)
+    );
 }
